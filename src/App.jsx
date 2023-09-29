488d622ebedb5273d83fc6542b3704f2d4d1bfa5
jhorhero~ <anas615330@gmail.com>
2023-09-29 16:28:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './Components/Header/header'
+import Blogs from './Components/Blogs/Blogs'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    <Header></Header>
+    <Blogs></Blogs>
+   
+    </>
+  )
+}
+
+export default App
